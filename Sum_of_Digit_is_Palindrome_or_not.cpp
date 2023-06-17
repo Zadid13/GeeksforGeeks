a88931c621f50cf9de828e251368f7867631dcef
Talukder Hasnat Zadid <77864700+Zadid13@users.noreply.github.com>
2023-06-17 15:26:40 +0600

    Update Sum_of_Digit_is_Palindrome_or_not.cpp

--- Sum_of_Digit_is_Palindrome_or_not.cpp
+++ Sum_of_Digit_is_Palindrome_or_not.cpp
@@ -37,3 +37,49 @@ int main()
 
     return 0;
 }
+
+
+
+
+
+
+
+//GFG Format
+/*#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void solve() {
+        int t;
+        cin >> t;
+        while (t--) {
+            string s;
+            cin >> s;
+            int digitSum = 0;
+            for (auto u : s) {
+                digitSum += (u - '0');
+            }
+            string str = to_string(digitSum);
+            string tmp = str;
+            reverse(tmp.begin(), tmp.end());
+            if (tmp == str) {
+                cout << "YES" << endl;
+            } else {
+                cout << "NO" << endl;
+            }
+        }
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    Solution obj;
+    obj.solve();
+
+    return 0;
+}
+*/
